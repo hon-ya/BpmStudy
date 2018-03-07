bb7dd32b9d630552986c757f46d8aab096c4cd83
hon_ya <spaceodyssey2008@gmail.com>
2018-03-07 22:54:16 +0900

    * 現在位置を示すマーカーを表示 * 現ノート位置を計算する方法を時間ベースに変更 * ユーザのビートをマイク入力から得て表示するように

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 class App {
-    constructor(container) {
+    constructor(container, mediaStream) {
+        let self = this;
+        
         this.container = container;
+        this.mediaStream = mediaStream;
         
         // create canvas
         this.canvas = document.createElement('canvas');
@@ -10,7 +13,6 @@ class App {
         this.canvasContext = this.canvas.getContext( '2d' );
 
         // register size change event
-        let self = this;
         function resizeCanvas(){
             self.canvas.width = window.innerWidth;
             self.canvas.height = window.innerHeight;
@@ -31,19 +33,33 @@ class App {
         this.interval = 25.0;
 
         // create audio context 
-        this.audioContext = new AudioContext();
-        
-        // initialize application settings
-        this.bpm = 80;  // beats per seconds
+        this.audioContext = new AudioContext({ latencyHint:'interactive', sampleRate:96000 });
+        this.scriptProcessor = this.audioContext.createScriptProcessor(256, 1, 1);
+        this.scriptProcessor.connect(this.audioContext.destination);
+        this.mediaStreamSource = this.audioContext.createMediaStreamSource(this.mediaStream);
+        this.mediaStreamSource.connect(this.scriptProcessor);
+
+        // initialize metronome settings
+        this.bpm = 120;  // beats per seconds
         this.quaterNoteCount = 4;
-        this.quaterNoteDivision = 2;
+        this.quaterNoteDivision = 1;
         this.noteSoundLength = 0.05;
         this.totalNoteCount = this.quaterNoteCount * this.quaterNoteDivision;
         this.secondsPerNote = 60 / this.bpm / this.quaterNoteDivision;
+        this.secondsPerBar = 60 / this.bpm * this.quaterNoteCount;
         this.scheduleMargin = 0.1;  // seconds
-        this.notesQueue = [];
         this.isPlaying = false;
-        
+        this.currentTime = 0;
+        this.currentPositionInBar = 0;
+        //this.microphoneInputDelay = 0;   // seconds
+        this.microphoneInputDelay = 0.12;   // for my pc setting
+
+        // initialize beat detector settings
+        this.beatsQueue = [];
+        this.lastBeatDetectedTime = 0;
+        this.beatThreshold = 0.10;
+        this.beatIntervalMin = 0.1;
+
         // start animation frame loop
         function frameLoop() {
             self.calc();
@@ -51,6 +67,11 @@ class App {
             requestAnimationFrame(frameLoop);
         }
         this.requestId = requestAnimationFrame(frameLoop);
+
+        // start beat detecting
+        this.scriptProcessor.onaudioprocess = function(event) {
+            self.updateBeats(event);
+        };
 
         // initialize user interface
         this.canvas.onclick = function(){
@@ -64,9 +85,10 @@ class App {
         }
         this.isPlaying = true;
 
-        this.currentNote = 0;
+        this.beatsQueue = [];
+        this.startTime = this.audioContext.currentTime + this.scheduleMargin;
         this.nextScheduleNote = 0;
-        this.nextScheduleNoteTime = this.audioContext.currentTime + this.secondsPerNote;
+        this.nextScheduleNoteTime = this.startTime;
         this.worker.postMessage({ command:"start", interval:this.interval });
     }
 
@@ -88,15 +110,35 @@ class App {
         }
     }
 
+    updateBeats(e) {
+        let inputs = new Float32Array(e.inputBuffer.length);
+        e.inputBuffer.copyFromChannel(inputs, 0, 0);
+
+        let maxValue = Math.max.apply(null, inputs);
+        let value = maxValue > this.beatThreshold ? maxValue : 0;
+
+        let currentTime = e.playbackTime;
+        if(value > 0 && (this.lastBeatDetectedTime + this.beatIntervalMin) < currentTime) {
+            this.beatsQueue.push({ volume:value, time:currentTime });
+            this.lastBeatDetectedTime = currentTime;
+        }
+    }
+
     calc() {
+        if(this.isPlaying)
+        {
+            this.currentTime = this.audioContext.currentTime;
+            this.currentPositionInBar = Math.max(0, ((this.currentTime - this.startTime) % this.secondsPerBar) / this.secondsPerBar);
+
+            if(this.microphoneInputDelay == 0 && this.beatsQueue.length > 1)
+            {
+                this.microphoneInputDelay = this.beatsQueue[0].time - this.startTime;
+                console.log(`microphoneInputDelay = ${this.microphoneInputDelay}`);
+            }
+        }
     }
 
     draw() {
-        while(this.notesQueue.length > 0 && this.notesQueue[0].time < this.audioContext.currentTime){
-            this.currentNote = this.notesQueue[0].note;
-            this.notesQueue.splice(0, 1);
-        }
-
         // clear screen
         this.canvasContext.fillStyle = "cornsilk";
         this.canvasContext.fillRect(0, 0, this.canvas.width, this.canvas.height);
@@ -108,25 +150,54 @@ class App {
         this.canvasContext.textAlign = "center";
         this.canvasContext.fillText(`${this.bpm}`, this.canvas.width / 2, this.canvas.height / 3);
 
-        // draw notes
-        let xunit = this.canvas.width / (this.totalNoteCount + 1);
-        let x = xunit;
-        let y = this.canvas.height / 4 * 3;
-        for(let i = 0; i < this.totalNoteCount; i++) {
-            let radius = 20;
-            if(i % this.quaterNoteDivision == 0) {
-                radius *= 2;
-            }
+        // draw marker
+        let xmargin = this.canvas.width / 10;
+        let barWidth = this.canvas.width - xmargin * 2;
+        let noteOffset = barWidth / this.totalNoteCount;
+        {
+            let x = this.currentPositionInBar * barWidth + xmargin;
+            let y = this.canvas.height / 4 * 3;
+            let radius = this.canvas.width / 200;
 
             this.canvasContext.beginPath();
-            this.canvasContext.arc(x + xunit * i, y, radius, 0, Math.PI * 2, false);
-            if(i == this.currentNote) {
-                this.canvasContext.fillStyle = "black";
-                this.canvasContext.fill();
+            this.canvasContext.arc(x, y, radius, 0, Math.PI * 2, false);
+            this.canvasContext.fill();
+        }
+
+        // draw notes
+        {
+            let radius_base = this.canvas.width / 100;
+            let currentNote = Math.floor(this.currentPositionInBar * this.totalNoteCount);
+            for(let i = 0; i < this.totalNoteCount + 1; i++) {
+                let x = xmargin + noteOffset * i;
+                let y = this.canvas.height / 4 * 3;
+                let radius = i % this.quaterNoteDivision == 0 ? radius_base * 2 : radius_base;
+
+                this.canvasContext.beginPath();
+                this.canvasContext.arc(x, y, radius, 0, Math.PI * 2, false);
+                if(i == currentNote) {
+                    this.canvasContext.fillStyle = "black";
+                    this.canvasContext.fill();
+                }
+                else {
+                    this.canvasContext.stroke();
+                }
             }
-            else {
-                this.canvasContext.stroke();
-            }
+        }
+
+        // draw user beats
+        let currentBarNumber = Math.floor((this.currentTime - this.startTime - this.microphoneInputDelay) / this.secondsPerBar);
+        for(let i = 0; i < this.beatsQueue.length; i++){
+            let time = this.beatsQueue[i].time;
+            let positionInBar = Math.max(0, ((time - this.startTime - this.microphoneInputDelay) % this.secondsPerBar) / this.secondsPerBar);
+            let barNumber = Math.floor((time - this.startTime - this.microphoneInputDelay) / this.secondsPerBar);
+            let radius = 10;
+            let x = positionInBar * barWidth + xmargin;
+            let y = this.canvas.height / 5 * 3 - (currentBarNumber - barNumber) * radius * 3;
+            
+            this.canvasContext.beginPath();
+            this.canvasContext.arc(x, y, radius, 0, Math.PI * 2, false);
+            this.canvasContext.fill();
         }
     }
 
@@ -153,8 +224,6 @@ class App {
         osc.start(this.nextScheduleNoteTime);
         osc.stop(this.nextScheduleNoteTime + this.noteSoundLength);
 
-        this.notesQueue.push({ note: this.nextScheduleNote, time: this.nextScheduleNoteTime });
-        
         this.nextScheduleNote = (this.nextScheduleNote + 1) % this.totalNoteCount;
         this.nextScheduleNoteTime += this.secondsPerNote;
     }
@@ -190,5 +259,16 @@ window.onload = function() {
         navigator.msGetUserMedia);
 
     let container = document.getElementById('container');
-    let app = new App(container);
+
+    navigator.getUserMedia (
+        {
+           audio: true
+        },
+        function(mediaStream) {
+            let app = new App(container, mediaStream);
+        },
+        function(e) {
+            alert("failed to getUserMedia(): " + e.name);
+         }
+    );
 }
